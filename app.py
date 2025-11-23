778425b5bb1927963455367eccb9e5060642b221
Phuong Pham <phuongthianhpham@usf.edu>
2025-11-23 11:22:47 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 hours = input("How many hours did you study today? ")
+#Convert the input into a numeric type (e.g., float) and perform at least one calculation.
 hours = float(hours)
 weekly_hours = hours * 7
 print(f"You are on track to study {weekly_hours} hours this week.")
+#basic error handling to prevent the program from crashing if the user enters a non-numeric value.
 try:
     hours = float(hours)
 except ValueError:
